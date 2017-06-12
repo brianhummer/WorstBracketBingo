7ee5e52180107e25183cbac03148f1cbb1b2e217
Brian Hummer <hummerb@illumengroup.com>
2017-06-12 12:02:50 -0500

    first bracket population, ui tweaks

--- WorstBracketBingo/Controllers/HomeController.cs
+++ WorstBracketBingo/Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using WorstBracketBingo.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace WorstBracketBingo.Controllers
 {
     public class HomeController : Controller
     {
-        public IActionResult Index()
+        private readonly BingoContext _context;
+
+        public HomeController(BingoContext context)
         {
-            return View();
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Brackets.ToListAsync());
         }
 
         public IActionResult About()
